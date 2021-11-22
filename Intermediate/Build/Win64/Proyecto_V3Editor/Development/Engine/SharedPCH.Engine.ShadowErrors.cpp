5a0ddec70de79bfedaee53e4393fd5d42b1590a0
Manuel Amaury Mendoza García <amg05@hotmail.es>
2021-11-22 09:59:41 -0600

    Steam Sessions

---
+++ Intermediate/Build/Win64/Proyecto_V3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30040
+#include "C:/Users/amg05/Documents/GitHub/VideojuegosONLINE/Intermediate/Build/Win64/Proyecto_V3Editor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/Proyecto_V3/Proyecto_V3.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Proyecto_V3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Proyecto_V3, "Proyecto_V3" );
